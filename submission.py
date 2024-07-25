58b492f5a414dba0cd457428829b0b635c9f2308
MarinaYanovskiy <marinay@campus.technion.ac.il>
2024-07-25 21:50:39 +0300

    alpha beta implementation- maybe needs some fixes

--- submission.py
+++ submission.py
@@ -35,7 +35,7 @@ def smart_heuristic(env: WarehouseEnv, robot_id: int):
             to_dest_cost = manhattan_distance(nearest_package.position, nearest_package.destination)
             path_cost = to_package_cost + to_dest_cost
 
-    return robot.credit * 75 + (reward / (path_cost + 1))+robot.battery
+    return robot.credit * 100 + (reward / (path_cost + 1))+robot.battery
 
 
 class AgentGreedyImproved(AgentGreedy):
@@ -46,7 +46,7 @@ class AgentGreedyImproved(AgentGreedy):
 class AgentMinimax(Agent):
     # TODO: section b : 1
     def run_step(self, env: WarehouseEnv, agent_id, time_limit):
-        self.end_time = time.time() + 0.7 * time_limit - 0.05
+        self.end_time = time.time() + 0.8 * time_limit - 0.05
         operators = env.get_legal_operators(agent_id)
         children = [env.clone() for _ in operators]
         for child, op in zip(children, operators):
@@ -96,7 +96,58 @@ class AgentMinimax(Agent):
 class AgentAlphaBeta(Agent):
     # TODO: section c : 1
     def run_step(self, env: WarehouseEnv, agent_id, time_limit):
-        raise NotImplementedError()
+        self.end_time = time.time() + 0.8 * time_limit - 0.05
+        operators = env.get_legal_operators(agent_id)
+        children = [env.clone() for _ in operators]
+        for child, op in zip(children, operators):
+            child.apply_operator(agent_id, op)
+        d = 2
+        step = operators[0]
+
+        while True:
+            if time.time() > self.end_time:
+                break
+            try:
+                children_results = [self.RB_AlphaBeta(child, 1 - agent_id, d - 1, agent_id, -float('inf'), float('inf')) for child in children]
+                max_result = max(children_results)
+                index_selected = children_results.index(max_result)
+                step = operators[index_selected]
+                d += 2
+            except TimeoutError:
+                break
+
+        return step
+
+    def RB_AlphaBeta(self, env, robot_id, d, turn, alpha, beta):
+        if time.time() > self.end_time:
+            raise TimeoutError
+
+        if env.done() or d == 0 or env.get_robot(1 - turn).battery == 0:
+            return smart_heuristic(env, robot_id)
+
+        operators = env.get_legal_operators(robot_id)
+        children = [env.clone() for _ in operators]
+        for child, op in zip(children, operators):
+            child.apply_operator(robot_id, op)
+
+        if turn == robot_id:
+            cur_max = - float('inf')
+            for child in children:
+                val = self.RB_AlphaBeta(child, 1 - robot_id, d - 1, turn, alpha, beta)
+                cur_max = max(val, cur_max)
+                alpha = max(cur_max, alpha)
+                if cur_max >= beta:
+                    break
+            return cur_max
+        else:
+            cur_min = float('inf')
+            for child in children:
+                val = self.RB_AlphaBeta(child, 1 - robot_id, d - 1, turn, alpha, beta)
+                cur_min = min(val, cur_min)
+                beta = min(cur_min, beta)
+                if cur_min <= alpha:
+                    break
+            return cur_min
 
 
 class AgentExpectimax(Agent):
